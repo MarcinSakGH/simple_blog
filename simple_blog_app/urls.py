d067f006c49e1e024fb255e79c1c217f5eba2cee
Marcin <marcin.sak.of@gmail.com>
2024-04-25 18:17:05 +0200

    Add delete view

--- simple_blog_app/urls.py
+++ simple_blog_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from simple_blog_app.views import PostListView, CreatePostView, PostDetailView
+from simple_blog_app.views import PostListView, CreatePostView, PostDetailView, RemovePostView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('create-post/', CreatePostView.as_view(), name='create-post'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail')
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('remove-post/<int:pk>/', RemovePostView.as_view(), name='remove-post'),
 ]

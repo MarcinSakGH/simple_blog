89b3b83ddd576de2c747cd765b85c43d2eae43b9
Marcin <marcin.sak.of@gmail.com>
2024-04-25 17:53:25 +0200

    Add post_detail view

--- simple_blog_app/urls.py
+++ simple_blog_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from simple_blog_app.views import PostListView
+from simple_blog_app.views import PostListView, CreatePostView, PostDetailView
 
 urlpatterns = [
-    path('post-list/', PostListView.as_view(), name='post-list'),
+    path('', PostListView.as_view(), name='post-list'),
+    path('create-post/', CreatePostView.as_view(), name='create-post'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail')
 ]
